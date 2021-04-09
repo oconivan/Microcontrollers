a37393704f605d280a7867e0548f8a77ba28ebe8
oconivan <62733756+oconivan@users.noreply.github.com>
2021-04-08 21:07:02 -0600

    Add files via upload

---
+++ MSP432 Labs/lab5_msp430/Debug/lab5_msp430.pp
@@ -0,0 +1,23 @@
+# FIXED
+
+lab5_msp430.obj: ../lab5_msp430.c
+lab5_msp430.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h
+lab5_msp430.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+lab5_msp430.obj: ../lcdLib.h
+lab5_msp430.obj: C:/ti/ccsv5/tools/compiler/msp430/include/string.h
+lab5_msp430.obj: C:/ti/ccsv5/tools/compiler/msp430/include/linkage.h
+lab5_msp430.obj: C:/ti/ccsv5/tools/compiler/msp430/include/stdio.h
+lab5_msp430.obj: C:/ti/ccsv5/tools/compiler/msp430/include/stdarg.h
+lab5_msp430.obj: C:/ti/ccsv5/tools/compiler/msp430/include/stdlib.h
+lab5_msp430.obj: C:/ti/ccsv5/tools/compiler/msp430/include/stdio.h
+
+../lab5_msp430.c: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+../lcdLib.h: 
+C:/ti/ccsv5/tools/compiler/msp430/include/string.h: 
+C:/ti/ccsv5/tools/compiler/msp430/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/msp430/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/msp430/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/msp430/include/stdlib.h: 
+C:/ti/ccsv5/tools/compiler/msp430/include/stdio.h: 
---
+++ MSP432 Labs/lab5_msp430/Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+C_SRCS += \
+../lab5_msp430.c \
+../lcdLib.c 
+
+C_DEPS += \
+./lab5_msp430.d \
+./lcdLib.d 
+
+OBJS += \
+./lab5_msp430.obj \
+./lcdLib.obj 
+
+OBJS__QUOTED += \
+"lab5_msp430.obj" \
+"lcdLib.obj" 
+
+C_DEPS__QUOTED += \
+"lab5_msp430.d" \
+"lcdLib.d" 
+
+C_SRCS__QUOTED += \
+"../lab5_msp430.c" \
+"../lcdLib.c" 
+
+
