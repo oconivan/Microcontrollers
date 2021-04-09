c8727724be77675ba1b15384f30f76bfb71a3800
oconivan <62733756+oconivan@users.noreply.github.com>
2021-04-08 21:18:08 -0600

    Add files via upload

---
+++ MSP432 Labs/lab8_Receiver/Debug/uartMsp.pp
@@ -0,0 +1,19 @@
+# FIXED
+
+uartMsp.obj: ../uartMsp.c
+uartMsp.obj: ../uartMsp.h
+uartMsp.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h
+uartMsp.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+uartMsp.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430.h
+uartMsp.obj: C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/stdio.h
+uartMsp.obj: C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/linkage.h
+uartMsp.obj: C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/stdarg.h
+
+../uartMsp.c: 
+../uartMsp.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/stdarg.h: 
