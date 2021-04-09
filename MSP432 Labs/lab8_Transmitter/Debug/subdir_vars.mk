8b870c19609e2d4989e1319517b40bdc827ee0c0
oconivan <62733756+oconivan@users.noreply.github.com>
2021-04-08 21:19:22 -0600

    Add files via upload

---
+++ MSP432 Labs/lab8_Transmitter/Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+C_SRCS += \
+../uartMsp.c \
+../uart_main_TX.c 
+
+C_DEPS += \
+./uartMsp.d \
+./uart_main_TX.d 
+
+OBJS += \
+./uartMsp.obj \
+./uart_main_TX.obj 
+
+OBJS__QUOTED += \
+"uartMsp.obj" \
+"uart_main_TX.obj" 
+
+C_DEPS__QUOTED += \
+"uartMsp.d" \
+"uart_main_TX.d" 
+
+C_SRCS__QUOTED += \
+"../uartMsp.c" \
+"../uart_main_TX.c" 
+
+
---
+++ MSP432 Labs/lab8_Transmitter/Debug/uart_main_TX.pp
@@ -0,0 +1,19 @@
+# FIXED
+
+uart_main_TX.obj: ../uart_main_TX.c
+uart_main_TX.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h
+uart_main_TX.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+uart_main_TX.obj: ../uartMsp.h
+uart_main_TX.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430.h
+uart_main_TX.obj: C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/stdio.h
+uart_main_TX.obj: C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/linkage.h
+uart_main_TX.obj: C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/stdarg.h
+
+../uart_main_TX.c: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+../uartMsp.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/stdarg.h: 
