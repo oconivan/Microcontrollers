cfa2df8308d9cfbad658c61caf64c7c8f277f098
oconivan <62733756+oconivan@users.noreply.github.com>
2021-04-08 20:57:29 -0600

    Add files via upload

---
+++ MSP432 Labs/lab2_msp430/Debug/subdir_vars.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+ASM_SRCS += \
+../lab2_msp430.asm 
+
+OBJS += \
+./lab2_msp430.obj 
+
+ASM_DEPS += \
+./lab2_msp430.pp 
+
+OBJS__QUOTED += \
+"lab2_msp430.obj" 
+
+ASM_DEPS__QUOTED += \
+"lab2_msp430.pp" 
+
+ASM_SRCS__QUOTED += \
+"../lab2_msp430.asm" 
+
+
